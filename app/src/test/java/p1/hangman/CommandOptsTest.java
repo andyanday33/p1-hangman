0786755713957667d7f5a39da93429924b8b1fa5
Berke Anday Baloglu <berke.andayb@gmail.com>
2022-02-08 16:45:50 +0300

    -- merge commits

--- app/src/test/java/p1/hangman/CommandOptsTest.java
+++ app/src/test/java/p1/hangman/CommandOptsTest.java
@@ -6,13 +6,13 @@ import org.junit.Test;
 
 public class CommandOptsTest {
 
-	@Test
-	public void optionsTest() {
-		String[] args = { "--guesses", "2", "--hints", "4", "words.txt" };
-		CommandOpts opts = new CommandOpts(args);
-		assertEquals(opts.getMaxguesses(), 2);
-		assertEquals(opts.getMaxhints(), 4);
-		assertEquals(opts.getWordsource(), "words.txt");
-	}
+    @Test
+    public void optionsTest() {
+        String[] args = { "--guesses", "2", "--hints", "4", "--source", "words.txt" };
+        CommandOpts opts = new CommandOpts(args);
+        assertEquals(opts.getMaxguesses(), 2);
+        assertEquals(opts.getMaxhints(), 4);
+        assertEquals(opts.getWordsource(), "words.txt");
+    }
 
 }
